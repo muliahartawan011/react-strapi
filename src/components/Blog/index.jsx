3668d471cd4536d9c9dbe28eaae11ee0c96eadfb
muliahartawan011 <mulia.negara@tokopedia.com>
2022-07-06 16:04:43 +0700

    revert changes

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -16,16 +16,6 @@ import { useParams } from 'react-router-dom';
 import moment from 'moment';
 
 function Blog() {
-  const [post, setPost] = useState();
-  const { id } = useParams();
-  useState(() => {
-    axios
-      .get(`http://localhost:1337/api/blogs/${id}?populate=*`)
-      .then(res => setPost(res.data.data))
-      .catch();
-  });
-
-  if (!!post) {
     return (
       <>
         <Box
@@ -34,25 +24,15 @@ function Blog() {
           alignItems="center"
           py="2rem"
         >
-          <Text fontSize="2xl" fontWeight='bold'>{post.attributes.title}</Text>
-          <Text fontStyle='italic' fontSize='xs' mr='auto' mb='2rem'>{moment(post.attributes.date).format("LL")}</Text>
-          <Image src={`http://localhost:1337${post.attributes.cover.data.attributes.url}`} mb='1rem' />
+          <Text fontSize="2xl" fontWeight='bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
+          <Text fontStyle='italic' fontSize='xs' mr='auto' mb='2rem'>{moment("05/06/2000").format("LL")}</Text>
+          <Image src='https://images.unsplash.com/photo-1468276311594-df7cb65d8df6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80' mb='1rem' />
 
-          <Text>{post.attributes.description}</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad eum quaerat eos consequuntur vel sint cupiditate, ut mollitia cumque! Natus similique rem nostrum omnis assumenda, molestias sint esse quidem.</Text>
         </Box>
       </>
     );
   }
 
-  return (
-    <>
-      <Stack>
-        <Skeleton height="20px" />
-        <Skeleton height="20px" />
-        <Skeleton height="20px" />
-      </Stack>
-    </>
-  );
-}
 
 export default Blog;
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -17,22 +17,11 @@ import { ArrowRight } from 'react-bootstrap-icons';
 import {Link} from 'react-router-dom'
 
 function ListBlogs() {
-  const [posts, setPosts] = useState();
-
-  useState(() => {
-    axios
-      .get('http://localhost:1337/api/blogs')
-      .then(res => setPosts(res.data.data))
-      .catch();
-  });
-
   const color = useColorModeValue('gray.100', 'gray.600');
   return (
     <>
       <Divider mb="5" />
       <List mb="8">
-        {!!posts ? (
-          posts.map(post => (
             <ListItem
             display="flex"
             flexDirection="column"
@@ -45,31 +34,27 @@ function ListBlogs() {
             }}
           >
             <Flex justifyContent="space-between" alignContent="center">
-              <Heading size="md">{post.attributes.title}</Heading>
+              <Heading size="md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Heading>
             </Flex>
             <HStack justifyContent="space-between">
               <Text fontSize="xs" fontStyle="italic">
-                {moment(post.attributes.date).format('LL')}
+                {moment("03/02/2013").format('LL')}
               </Text>
             </HStack>
 
             <Text noOfLines={3}>
-              {post.attributes.description}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quisquam animi quae. Cumque quidem quam aliquam, quaerat odit fuga aliquid saepe, expedita provident voluptate sit sint fugit eveniet cupiditate facilis!
             </Text>
 
             <Flex flexDirection="row" alignItems='center' gap='2' justifyContent="end">
               <Link
-                to={`/blog/${post.id}`}
+                to={`/blog/1`}
               >
                 Read More 
               </Link>
               <ArrowRight mx="10px" />
             </Flex>
-          </ListItem>
-          ))
-        ) : (
-          <p>kosong</p>
-        )}
+          </ListItem>        
       </List>
     </>
   );
